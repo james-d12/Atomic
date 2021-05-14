a28b50781d8006984038a7635b443c116d6aebf7
james <james_d02@protonmail.com>
2021-05-14 20:26:46 +0100

    fixed workflow

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -28,6 +28,7 @@ function(SetProjectWarnings project_name)
       /w14906 # string literal cast to 'LPWSTR'
       /w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
       /permissive- # standards conformance mode for MSVC compiler.
+      /EHsc # Unwind semantics
   )
 
   set(CLANG_WARNINGS
