a51a2e2df36cf607402f8be1e92af51c63bb03bf
james <james_d02@protonmail.com>
2021-05-24 20:34:44 +0100

    reverted new workflow & updated tools

--- tools/build_documentation.ps1
+++ tools/build_documentation.ps1
@@ -1,6 +1,11 @@
 
 $currentLocation = (Get-Location | Out-String)
 
-Write-Output $currentLocation
+Get-ChildItem -Path "./Dokky" -Recurse | Remove-Item -Force -Recurse -Confirm:$false
+Remove-Item "./Dokky" -Recurse -Force -Confirm:$false
 
-#git clone https://github.com/james-d12/Dokky.git
+git clone https://github.com/james-d12/Dokky.git
+Copy-Item ./docgen.json -Destination "./Dokky" 
+Set-Location "./Dokky"
+python ./generator.py ./docgen.json 
+Set-Location "../"
