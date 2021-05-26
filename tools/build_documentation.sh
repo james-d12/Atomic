c76e65bd7f896fc0162d6f65372ea5a6ab808312
james <james_d02@protonmail.com>
2021-05-26 23:01:46 +0100

    updated workflows

--- tools/build_documentation.sh
+++ tools/build_documentation.sh
@@ -1,28 +1,11 @@
-#!/usr/bin/bash 
+#!/bin/bash 
 
-check_directory_and_dependencies(){
-    sourceFolder="$(pwd)/include"
+if [ ! -d "./Dokky" ]; then 
+	command -v git &> /dev/null && git clone https://github.com/james-d12/Dokky.git 
+fi 
 
-    if [[ "${sourceFolder: -14}" != "Atomic/include" ]]; then
-        echo "Please run in Atomic's Root Directory."
-        exit 1
-    fi 
+cp ./docgen.json "./Dokky/"
+cd "./Dokky"
+python ./generator.py ./docgen.json 
+cd "../"
 
-    if ! command -v git &> /dev/null; then
-        echo "Git not installed."
-        exit 1
-    fi
-
-    if ! command -v python &> /dev/null; then
-        echo "Python not installed."
-        exit 1
-    fi
-}
-
-generate_documentation(){
-    git clone https://github.com/james-d12/Dokky.git 
-    python ./Dokky/generator.py ../docgen.json
-}
-
-check_directory_and_dependencies
-generate_documentation
